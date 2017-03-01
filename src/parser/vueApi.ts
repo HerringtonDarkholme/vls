0b74aa4a6ef3fb55809e623b169d242c910f5be2
Pine Wu <octref@gmail.com>
2017-03-01 12:34:59 -0500

    Remove razor and ionic, add vue

---
+++ src/parser/vueApi.ts
@@ -0,0 +1,11 @@
+/*
+var customTags: { [tag: string]: string[] } = {
+  input: ['v-model', 'v-required', 'v-minlength', 'v-maxlength', 'v-pattern', 'v-trim'],
+  select: ['v-model'],
+  textarea: ['v-model', 'v-required', 'v-minlength', 'v-maxlength', 'v-pattern', 'v-trim']
+};
+*/
+
+export const vueDirectives = [
+  "v-text", "v-html", "v-show", "v-if", "v-else", "v-else-if", "v-for", "v-on", "v-bind", "v-model", "v-pre", "v-cloak", "v-once",
+];
--- src/services/tagProviders.ts
+++ src/services/tagProviders.ts
@@ -4,12 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-import {getHTML5TagProvider, getAngularTagProvider, getIonicTagProvider, IHTMLTagProvider} from '../parser/htmlTags';
-import {getRazorTagProvider} from '../parser/razorTags';
+import {getHTML5TagProvider, getVueTagProvider, IHTMLTagProvider} from '../parser/htmlTags';
 
 export let allTagProviders : IHTMLTagProvider[] = [
   getHTML5TagProvider(),
-  getAngularTagProvider(),
-  getIonicTagProvider(),
-  getRazorTagProvider()
+  getVueTagProvider()
 ];
