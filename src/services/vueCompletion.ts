6fec65f799b6e2d46084bb2916225ade5805e792
Pine Wu <octref@gmail.com>
2017-05-07 20:57:02 -0400

    Add top level completions

--- src/services/vueCompletion.ts
+++ src/services/vueCompletion.ts
@@ -1,33 +1,147 @@
-import {TextDocument, Position, CompletionList, CompletionItemKind, Range, TextEdit, InsertTextFormat, CompletionItem} from 'vscode-languageserver-types';
-import {HTMLDocument} from '../parser/htmlParser';
-import {TokenType, createScanner, ScannerState} from '../parser/htmlScanner';
-import {allTagProviders} from './tagProviders';
-import {CompletionConfiguration} from '../htmlLanguageService';
+import {CompletionList, CompletionItemKind, InsertTextFormat, CompletionItem} from 'vscode-languageserver-types';
 
-// Top level vue completion
 export function doVueComplete(): CompletionList {
-  const result: CompletionList = {
-    isIncomplete: false,
-    items: []
-  };
-
-  result.items.push({
-    label: 'scaffold',
-    insertTextFormat: InsertTextFormat.Snippet,
-    insertText: 
+  const topLevelCompletions: CompletionItem[] = [
+    {
+      label: 'scaffold',
+      documentation: 'Scaffold <template>, <script> and <style>',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
 `<template>
-
+  \${}
 </template>
 
 <script>
+export default {
 
+}
 </script>
 
 <style>
 
 </style>
 `
-  });
+    },
+    {
+      label: 'template with html',
+      documentation: 'Scaffold <template> with html',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<template>
+  \${}
+</template>
+`
+    },
+    {
+      label: 'template with pug',
+      documentation: 'Scaffold <template> with pug',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<template lang="pug">
+  \${}
+</template>
+`
+    },
+    {
+      label: 'script with JavaScript',
+      documentation: 'Scaffold <script> with JavaScript',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<script>
+export default {
+  \${}
+}
+</script>
+`
+    },
+    {
+      label: 'script with TypeScript',
+      documentation: 'Scaffold <script> with TypeScript',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<script lang="ts">
+export default {
+  \${}
+}
+</script>
+`
+    },
+    {
+      label: 'style with CSS',
+      documentation: 'Scaffold <style> with CSS',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<style>
+  \${}
+</style>
+`
+    },
+    {
+      label: 'style with scss',
+      documentation: 'Scaffold <style> with scss',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<style lang="scss">
+  \${}
+</style>
+`
+    },
+    {
+      label: 'style with less',
+      documentation: 'Scaffold <style> with less',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<style lang="less">
+  \${}
+</style>
+`
+    },
+    {
+      label: 'style with sass',
+      documentation: 'Scaffold <style> with sass',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<style lang="sass">
+  \${}
+</style>
+`
+    },
+    {
+      label: 'style with postcss',
+      documentation: 'Scaffold <style> with postcss',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<style lang="postcss">
+  \${}
+</style>
+`
+    },
+    {
+      label: 'style with stylus',
+      documentation: 'Scaffold <style> with stylus',
+      kind: CompletionItemKind.Snippet,
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertText: 
+`<style lang="stylus">
+  \${}
+</style>
+`
+    },
 
-  return result;
+  ];
+
+  return {
+    isIncomplete: false,
+    items: topLevelCompletions
+  };
 }
